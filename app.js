8adf3f21bf39278c5444a11480e522a75b52260f
harigustave <harigustave@gmail.com>
2023-12-28 21:33:13 +0000

    Adding notes app functionalities add, remove, list and read

--- app.js
+++ app.js
@@ -1,7 +1,59 @@
 const chalk=require('chalk');
+const yargs=require('yargs');
 const getNotes=require('./notes');
 
-const mynotes=getNotes();
+const notes=[];
 
-console.log(mynotes);
-console.log(chalk.bold.inverse.green('Succes!'));
+// Add note
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    builder:{
+        title:{
+            describe:'Note title',
+            demandOption:true,
+            type: 'string'
+        },
+
+        body:{
+            describe:'Note bbody',
+            demandOption:true,
+            type:'string'
+        }
+    },
+    handler:function(argv){
+        console.log('Adding a new note...')
+        console.log('Title:'+argv.title)
+        console.log('Body:'+argv.body)
+    }
+})
+
+//Remove command
+yargs.command({
+    command:'remove',
+    describe: 'Remove a note',
+    handler:function(){
+        console.log('Removing a note...')
+    }
+})
+
+//List command
+yargs.command({
+    command:'list',
+    describe: 'List all notes',
+    handler:function(){
+        console.log('Listing all notes...')
+    }
+})
+
+//Read command
+yargs.command({
+    command:'read',
+    describe: 'Read a note',
+    handler:function(){
+        console.log('Reading a note...')
+    }
+})
+
+console.log(yargs.argv);
+
